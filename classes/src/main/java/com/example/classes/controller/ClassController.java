991aa6771d9d1474fc1fea4c990a1eb6991406f6
Rhuan Kowic <rhuanksantos2@gmail.com>
2023-10-27 13:24:30 -0300

    Nome dos arquivos foram modificados.

--- classes/src/main/java/com/example/classes/controller/ClassController.java
+++ classes/src/main/java/com/example/classes/controller/ClassController.java
@@ -1,4 +1,4 @@
 package main.java.com.example.classes.controller;
-public class TaskController {
+public class ClassController {
   
 }
---
+++ classes/src/main/java/com/example/classes/repository/ClassRepository.java
@@ -0,0 +1,11 @@
+package main.java.com.example.classes.repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import main.java.com.example.classes.model.Class;
+
+public interface ClassRepository {
+
+  
+
+}
