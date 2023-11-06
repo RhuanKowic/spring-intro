6f503e01c7c2403db5f4ffbdd6e3cb9b7a66b0aa
Rhuan Kowic <rhuanksantos2@gmail.com>
2023-11-06 19:23:07 -0300

    Criando o repositorio do Product.

---
+++ product/src/main/java/com/product/repository/ProductRepository.java
@@ -0,0 +1,16 @@
+package com.product.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.product.model.Product;
+import com.product.model.Category;
+public interface ProductRepository  extends JpaRepository<Product, Long>{
+    List<Product> findByCategory(Category category);
+
+    @Query(value = "SELECT * FROM product p where p.price < :maxPrice", nativeQuery = true)
+    List<Product> findProductsBelowMaxPrice(@Param("maxPrice") double maxPrice);
+}
