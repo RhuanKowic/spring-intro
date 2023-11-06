f189db7990c95b1b790c5eb0055179dc618a3e10
Rhuan Kowic <rhuanksantos2@gmail.com>
2023-11-06 19:20:20 -0300

    Implementando a entidade Product.

---
+++ product/src/main/java/com/product/model/Product.java
@@ -0,0 +1,48 @@
+package com.product.model;
+
+import jakarta.persistence.*;
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, name = "name")
+    private String name;
+
+    @Column(nullable = false, name =  "price")
+    private double price;
+
+    @ManyToOne
+    @JoinColumn(name = "categoru_id")
+    private Category category;
+
+    public Long getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public double getPrice(){
+        return price;
+    }
+
+    public void setPrice(double price){
+        this.price = price;
+    }
+
+    public Category getCategory(){
+        return category;
+    }
+
+    public void setCategory(Category category){
+        this.category = category;
+    }
+
+}
